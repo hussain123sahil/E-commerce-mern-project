e35e946c98affde8831e92f17bfb3d54d0246421
sahil <sahilhussain1231@gmail.com>
2024-03-15 23:43:48 +0530

    added schema for database

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const connectDatabase = require("./config/database")
 dotenv.config({path:"backend/config/config.env"});
 
 //connecting to database
-connectDatabase() 
+connectDatabase()
 
 app.listen(process.env.PORT,()=>{
 
